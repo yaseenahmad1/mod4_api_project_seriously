e45d76cf81992b8737b18147c1a04cc7e719b472
yaseenahmad1 <yahmad09@gmail.com>
2025-05-07 15:47:32 -0700

    phase 1: user login thunk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,21 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'; // Added this line from PHASE 1 
 
 const store = configureStore();
 // add this line 
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions; // Added this line from PHASE 1 
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
+  server: { 
+    proxy: { 
+      '/api': 'http://localhost:8000' // this proxy will force the frontend server to act like it's being served from the backend server. 
+  // So if you do a fetch request in the React frontend like fetch('api/csrf/restore), then GET /api/csrf/restore request will be made to the backend server instead of the frontend server. 
+    },
+  }
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
   // server: {
